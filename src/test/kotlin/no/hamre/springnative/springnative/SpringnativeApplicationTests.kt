3e6c720bc1c12d0e114f847ea687151b995be0f8
Bjørn Hamre <bha@ambita.com>
2023-07-17 15:46:02 +0200

    Initial project

---
+++ src/test/kotlin/no/hamre/springnative/springnative/SpringnativeApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.hamre.springnative.springnative
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringnativeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
